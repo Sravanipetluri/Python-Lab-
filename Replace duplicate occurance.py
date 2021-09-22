5746788e676ca184b22de4a74440dc4491c50199
Sravanipetluri <79572520+Sravanipetluri@users.noreply.github.com>
2021-09-22 14:12:36 +0530

    Update Replace duplicate occurance.py

--- Replace duplicate occurance.py
+++ Replace duplicate occurance.py
@@ -1,13 +1,6 @@
 test_string = 'James is good in Science , Science is James favourite subject. '
-  
-# printing original string
 print("The original string is : " + str(test_string))
-  
-# initializing replace mapping 
 repl_dict = {'James' :  'his', 'Science' : 'it' }
-  
-# Replace duplicate Occurrence in String
-# Using keys() + index() + list comprehension
 test_list = test_string.split(' ')
 res = ' '.join([repl_dict.get(val) if val in repl_dict.keys() and test_list.index(val) != index 
                                    else val for index, val in enumerate(test_list)])
